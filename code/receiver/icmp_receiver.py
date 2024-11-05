b59b6c685b7060468fa0046db1a9adc53c690594
ahmetysnocak <ahmetysnocak@gmail.com>
2024-11-05 19:24:03 +0300

    .

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,9 @@
-import scapy
+import scapy.all as scp
 
-# Implement your ICMP receiver here
+#Implement your ICMP receiver here
+
+scp.sniff(filter="icmp",prn = lambda x: x.show(),count = 1)
+
+
+
+
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,11 @@
-import scapy
+import scapy.all as scp
 
 # Implement your ICMP sender here
+packet = scp.IP(dst="receiver",ttl=1)/scp.ICMP()
+scp.send(packet)
+
+
+
+
+
+
